6ec31fdfb9b7e20208650fd1cc82eb4884503aa3
François Duforest <francois.duforest@gmail.com>
2022-05-07 16:47:29 +0200

    [US 2] Add Buy & Resell functions on marketplace contract

--- contracts/NFTmarketplace.sol
+++ contracts/NFTmarketplace.sol
@@ -17,20 +17,56 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, ERC721URIStorage {
     // using is used for including a library within a contract in solidity
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
     mapping(string => uint8) hashes;
     address contractAddress;
+    uint256 listingPrice = 0.025 ether;
 
-    constructor(address marketplaceAddress)
+    struct NftItem {
+        uint256 itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable creator;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool sold;
+    }
+
+    mapping(uint256 => NftItem) private nftItemIds;
+
+    event nftItemAdded(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address creator,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    event sale(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address owner,
+        address seller,
+        uint256 price
+    );
+
+    constructor()
+        /*address marketplaceAddress*/
         ERC721("MoonRocket Tokens ", "MORT")
     {
-        contractAddress = marketplaceAddress;
+        //contractAddress = marketplaceAddress;
         // owner = payable(msg.sender);
     }
 
     // ::::::::::::: MINT ::::::::::::: //
 
     /// @notice Function for allowing an address to mint an NFT & authorize the marketplace to transfer sold items from sender address to the buyer's address.
-    /// @param _to The address of the voter
+    /// @param _to The address of the recipient (owner of the nft)
     /// @param _hash The CID hash
     /// @param _metadata The json metadata CID hash
     /// @return tokenId The NFT token id
@@ -43,7 +79,7 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, ERC721URIStorage {
         require(hashes[_hash] != 1);
         hashes[_hash] = 1;
 
-        // Attribute a unique token Id
+        // Set a unique token Id
         _tokenIds.increment();
         uint256 tokenId = _tokenIds.current();
 
@@ -55,5 +91,51 @@ contract NFTMarketplace is ReentrancyGuard, Ownable, ERC721URIStorage {
         setApprovalForAll(contractAddress, true);
 
         return tokenId;
+    }
+
+    // ::::::::::::: ADD NFT ITEM ON THE MARKETPLACE ::::::::::::: //
+
+    /// @notice Function for setting NFT item properties put it on the marketplace.
+    /// @param _nftContract The address of the contract
+    /// @param _tokenId The NFT token id
+    /// @param _price The price of the NFT item
+    function PutNftItemOnMarket(
+        address _nftContract,
+        uint256 _tokenId,
+        uint256 _price
+    ) public payable nonReentrant {
+        require(_price > 0, "Price must not be 0");
+        require(
+            msg.value > listingPrice,
+            "Price must be equal to listing price"
+        );
+
+        _itemIds.increment();
+        uint256 _itemId = _itemIds.current();
+
+        // Transfer the ownership of the NFT item to the marketplace contract allowing it to sell the item
+        IERC721(_nftContract).transferFrom(msg.sender, address(this), _tokenId);
+
+        // Add the Nft item to the nftItemIds array, set it an id and the marketplace as the new owner
+        nftItemIds[_itemId] = NftItem(
+            _itemId,
+            _nftContract,
+            _tokenId,
+            payable(msg.sender),
+            payable(msg.sender),
+            payable(address(0)),
+            _price,
+            false
+        );
+
+        emit nftItemAdded(
+            _itemId,
+            _nftContract,
+            _tokenId,
+            msg.sender,
+            address(0),
+            _price,
+            false
+        );
     }
 }
--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,5 @@
 var NFTmarketplace = artifacts.require("./NFTmarketplace.sol")
 
 module.exports = function (deployer) {
-  deployer.deploy(NFTmarketplace, NFTmarketplace.address)
+  deployer.deploy(NFTmarketplace)
 }
