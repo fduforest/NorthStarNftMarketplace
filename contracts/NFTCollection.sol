544aca7dfd2601dac3a084eda10b0245049fbc3c
Pire Yohan <74233754+Pireyohan@users.noreply.github.com>
2022-05-09 22:27:08 +0200

    Create NFTCollection.sol

---
+++ contracts/NFTCollection.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "../node_modules/@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+/// @title NFTCollection contract
+/// @author Pire Yohan  , Duforest François, Khoury Alexandre
+
+
+contract NFTCollection is Initializable, ERC721URIStorageUpgradeable {
+
+}
