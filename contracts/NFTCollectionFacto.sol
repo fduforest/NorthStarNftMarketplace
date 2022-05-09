60d61e632d3ca80a813b73ca627f99b0c37733d4
Pire Yohan <74233754+Pireyohan@users.noreply.github.com>
2022-05-09 22:48:57 +0200

    A faire , TP  Collection Facto 09/05 22:48

---
+++ contracts/NFTCollectionFacto.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "./NFTCollection.sol";
+
+/// @title NFTCollectionFactory contract
+/// @author Pire Yohan  , Duforest François, Khoury Alexandre
+/// @notice This contract allows to initialize collection of NFTs
+/// @dev The contract code contains comments for developers only visible in the source code
+
+contract NFTCollectionFactory {
+  /// Capter le factory demain 
+  
+ ///function create un fact
+ /// use assembly
+ /// create2 
+ /// Voir tp fait tantot
+}
